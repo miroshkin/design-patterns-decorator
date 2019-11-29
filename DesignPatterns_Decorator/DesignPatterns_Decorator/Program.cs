ad6c4b307676ec99dfcbb0a833277d134441690a
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-11-29 18:42:30 +0300

    Added initial project

---
+++ DesignPatterns_Decorator/DesignPatterns_Decorator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns_Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Design Patterns - Decorator!");
+        }
+    }
+}
